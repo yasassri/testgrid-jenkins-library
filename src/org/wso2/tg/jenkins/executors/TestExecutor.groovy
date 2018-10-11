b05658459e87c451d529e592692f258b3e795582
Yasassri <ycrnet@gmail.com>
2018-10-11 11:05:13 +0530

    Update test executor

--- src/org/wso2/tg/jenkins/executors/TestExecutor.groovy
+++ src/org/wso2/tg/jenkins/executors/TestExecutor.groovy
@@ -26,6 +26,11 @@ def runPlan(tPlan, testPlanId) {
     def commonUtil = new Common()
     def notfier = new Slack()
     def awsHelper = new AWSUtils()
+    dir("${PWD}/${testPlanId}") {
+        unstash name: "${JOB_CONFIG_YAML}"
+        unstash name: "test-plans"
+        unstash name: "TestGridYaml"
+    }
     sh """
         echo Executing Test Plan : ${tPlan} On directory : ${testPlanId}
         echo Creating workspace and builds sub-directories
@@ -43,12 +48,7 @@ def runPlan(tPlan, testPlanId) {
 
         echo Unstashing test-plans and testgrid.yaml to ${PWD}/${testPlanId}
     """
-    
-    dir("${PWD}/${testPlanId}") {
-        unstash name: "${JOB_CONFIG_YAML}"
-        unstash name: "test-plans"
-        unstash name: "TestGridYaml"
-    }
+
 
     sh """
         cp /testgrid/testgrid-prod-key.pem ${PWD}/${testPlanId}/workspace/testgrid-key.pem
