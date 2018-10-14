bd7a315c5c2f30edbc9c60115d7d0be38b74e6a2
Yasassri <ycrnet@gmail.com>
2018-10-14 20:56:50 +0530

    Update Runner pipeline

--- vars/Runner.groovy
+++ vars/Runner.groovy
@@ -57,7 +57,7 @@ def call(def ab) {
                             echo "1111"
                             echo ab.toString()
                             echo "2222"
-                            echo ab.getRawBuild().toString()
+                            echo ab.getRawBuild()
                             echo "3333"
                             echo ab.getBuildVariables().toString()
                             echo "4444"
