a0f161f17b17d0b08c21a482810a30bea2cbee45
Yasassri <ycrnet@gmail.com>
2018-10-15 11:22:44 +0530

    Update Runner pipeline

--- src/org/wso2/tg/jenkins/util/RuntimeUtil.groovy
+++ src/org/wso2/tg/jenkins/util/RuntimeUtil.groovy
@@ -7,11 +7,11 @@ import org.wso2.tg.jenkins.Properties
  */
 
 def increaseTestGridRuntimeMemory(min, max) {
-//    def props = Properties.instance
-//    sh """
-//          echo ${props.TESTGRID_NAME}
-//          cd ${props.TESTGRID_DIST_LOCATION}
-//          cd ${props.TESTGRID_NAME}
-//          sed -i 's/-Xms256m -Xmx1024m/-Xmx${min} -Xms${max}/g' testgrid
-//        """
+    def props = Properties.instance
+    sh """
+          echo ${props.TESTGRID_NAME}
+          cd ${props.TESTGRID_DIST_LOCATION}
+          cd ${props.TESTGRID_NAME}
+          sed -i 's/-Xms256m -Xmx1024m/-Xmx${min} -Xms${max}/g' testgrid
+        """
 }
